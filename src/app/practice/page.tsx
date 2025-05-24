238acbd6be151212a5a76b1289b17c722af64dd9
Kaylie Mei <kayliemei@Kaylies-MacBook-Air.local>
2025-05-23 19:15:32 -0500

    removed title on practice page

--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -6,7 +6,7 @@ import Generate from "../generate";
 export default function Upload() {
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-4">Upload Your Quiz</h1>
+      <h1 className="text-2xl font-bold mb-4"></h1>
       <Generate />
     </div>
   );
